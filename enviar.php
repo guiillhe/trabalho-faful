f555380cac2a2d80c2478a8b90c4c22a3e4162b6
Anderson <andersonmeurerr@gmail.com>
2023-03-17 21:09:09 -0300

    Feito alterações para conectar no banco de dados mysql

--- enviar.php
+++ enviar.php
@@ -1,5 +1,4 @@
 <?php
-
 // Dados do formulário
 $nome = $_POST['nome'];
 $email = $_POST['email'];
@@ -7,38 +6,26 @@ $telefone = $_POST['telefone'];
 $assunto = $_POST['assunto'];
 $mensagem = $_POST['mensagem'];
 
-// Configuração da conexão com o Supabase
-$url = 'https://esbloxxkfsrwjnmpsswv.supabase.co/rest/v1/paper202301';
-$chave_api = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVzYmxveHhrZnNyd2pubXBzc3d2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Nzg4MjgyNTQsImV4cCI6MTk5NDQwNDI1NH0.hPvE1u80K-xbr71VTzlPnlww_eA1_zjCWylKUWPaJus';
-
-if (extension_loaded('openssl')) {
-    echo 'OpenSSL está habilitado';
-} else {
-    echo 'OpenSSL não está habilitado';
-}
-$dados = [
-    'nome' => $nome,
-    'email' => $email,
-    'telefone' => $telefone,
-    'assunto' => $assunto,
-    'mensagem' => $mensagem
-];
-$options = [
-    'http' => [
-        'header'  => "Content-type: application/json\r\nAuthorization: Bearer " . $chave_api,
-        'method'  => 'POST',
-        'content' => json_encode($dados),
-    ],
-];
-
-// Envio dos dados para o Supabase
-$context  = stream_context_create($options);
-$result = file_get_contents($url, false, $context);
-
-if ($result === false) {
-    // Tratar erro na conexão
-    echo "Erro na conexão com o Supabase";
-} else {
+    $servidor = "localhost";
+    $usuario = "root";
+    $senha = "";
+    $bancodedados = "uniasselvi20230313";
+	
+	$conexao = new mysqli($servidor, $usuario, $senha, $bancodedados);
+	
+	if ($conexao->connect_error) {
+    die("A conexão falhou: " + $conexao->conexaoect_error);
+    }
+	
+	$sql = "INSERT INTO CONTATO 
+	(CODIGO, NOME, EMAIL, MENSAGEM) VALUES 
+	(coalesce((SELECT MAX(c2.CODIGO) FROM CONTATO c2), 0) + 1,'".$nome."','".$email."','".$mensagem."') ";
+	
+	if (!($conexao->query($sql) === TRUE)) {
+        $conexao->close();
+        die("Erro: " . $sql . "<br>" . $conexao->error);
+    }
+	
     // Tratar sucesso no envio dos dados
-    echo "Dados enviados com sucesso para o Supabase";
-}
+    echo "Dados enviados com sucesso para o Mysql";
+?>
